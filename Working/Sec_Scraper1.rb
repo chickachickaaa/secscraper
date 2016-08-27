3eac3d4d75e234303e60a05c0d517745a11a6fae
T higgins <higgins.taylor.a@gmail.com>
2016-08-27 14:36:10 -0400

    sec_scraper.rb

---
+++ Working/Sec_Scraper1.rb
@@ -0,0 +1,26 @@
+require 'Nokogiri'
+require 'open-uri'
+require 'net/http'
+
+doc = Nokogiri::HTML(open("https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=8-k&SIC=&State=&Country=&CIK=&owner=include&accno=&start=0&count=900000000000000000000000000000000000000000"))
+href = []
+links = doc.css('a')
+links.each do |link|
+  if link['href'] =~ /[.][t][x][t]/
+    href << link['href']
+  end
+end
+i = 0
+while href.size > i do
+  url = "https://www.sec.gov#{href[i]}"
+  file = open(url)
+  file2 = open(url)
+  first_word = file.grep(/covenant/)
+  if first_word.size > 0
+      second_word = file2.grep(/violated | violation | non-compliance | out of compliance/)
+    if second_word.size > 0
+      p url
+    end
+  end
+  i+=1
+end
