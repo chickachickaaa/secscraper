2c011ecad1d7fea6bc62a01768e8c03c66872551
T higgins <higgins.taylor.a@gmail.com>
2016-08-27 15:19:46 -0400

    phrase from word

--- Working/Sec_Scraper.rb
+++ Working/Sec_Scraper.rb
@@ -2,7 +2,7 @@ require 'Nokogiri'
 require 'open-uri'
 require 'net/http'
 
-doc = Nokogiri::HTML(open("https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=8-k&SIC=&State=&Country=&CIK=&owner=include&accno=&start=100&count=200"))
+doc = Nokogiri::HTML(open("https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=8-k&SIC=&State=&Country=&CIK=&owner=include&accno=&start=0&count=100"))
 href = []
 links = doc.css('a')
 links.each do |link|
@@ -14,11 +14,8 @@ i = 0
 while href.size > i do
   url = "https://www.sec.gov#{href[i]}"
   file = open(url)
-  file2 = open(url)
-  first_word = file.grep(/covenant/)
-  if first_word.size > 0
-      second_word = file2.grep(/violated | violation | non-compliance | out of compliance/)
-    if second_word.size > 0
+  text = file.grep(/"Until the Company achieves sustained profitability" | "does not have sufficient capital to meet its needs" | "continues to seek loans or equity placements to cover such cash needs" | "there can be no assurance that any additional funds will be available to cover expenses as they may be incurred" | "unable to raise additional capital" | "may be required to take additional measures to conserve liquidity" | "Suspending the pursuit of its business plan" | "The Company may need additional financing" | "We will incur expenses in connection with our SEC filing requirements and we may not be able to meet such costs" | "could jeopardize our filing status with the SEC"/)
+    if text.size > 0
       p url
     end
   end
